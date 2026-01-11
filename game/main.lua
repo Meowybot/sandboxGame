87cd50d6d4a2d59cbc70bde4c9a76d14e10f8f91
Meowybot <jorgedidier149@gmail.com>
2026-01-11 12:03:05 -0600

    Update main.lua

--- game/main.lua
+++ game/main.lua
@@ -3,7 +3,7 @@ print("game :Dll")
 function love.load()
     print("loading game")
     require("state")
-    love.filesystem.mount(love.filesystem.getSourceBaseDirectory() .. "/assets", "assets", true) -- assets are in the source base directory
+    love.filesystem.mount(love.filesystem.getSourceBaseDirectory() .. "/mods", "mods", true) -- assets are in the source base directory
     print(love.filesystem.getSourceBaseDirectory())
     state.switch("mainmenu")
 end
