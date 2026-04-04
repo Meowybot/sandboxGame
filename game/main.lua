5b5864f337f1b14e88f149cde6c6e01fb129800d
Meowybot <jorgedidier149@gmail.com>
2026-04-04 10:23:18 -0600

    Delete main.lua

--- game/main.lua
+++
@@ -1,9 +0,0 @@
-print("game :Dll")
-
-function love.load()
-    print("loading game")
-    require("state")
-    love.filesystem.mount(love.filesystem.getSourceBaseDirectory() .. "/mods", "mods", true) -- assets are in the source base directory
-    print(love.filesystem.getSourceBaseDirectory())
-    state.switch("mainmenu")
-end
