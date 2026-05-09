44dfafa2d1d13ac008aa72e89c9bd722a8a325c6
Meowybot <jorgedidier149@gmail.com>
2026-05-09 11:24:53 -0600

    Create chunkio.lua

---
+++ game/lib/mSandGame/chunkio.lua
@@ -0,0 +1,12 @@
+local bytbin = require("lib.mBin.bytbin")
+local chunk = require("lib.mSandGame.chunk")
+
+local chunkio = {}
+
+function chunkio.save(c, worldpath)
+end
+
+function chunkio.load(worldpath, cx, cy)
+end
+
+return chunkio
